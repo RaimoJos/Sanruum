1ef4a7123bbcd74fb8a677497e6f9682b3ea30bb
[RaimoJos] <[sanruumou@gmail.com]>
2025-02-08 02:21:58 +0200

    updated.

--- sanruum/ai_core/speech/text_to_speech.py
+++ sanruum/ai_core/speech/text_to_speech.py
@@ -1,7 +1,9 @@
 # sanruum\ai_core\speech\text_to_speech.py
+from __future__ import annotations
+
 import os
 import tempfile
-from typing import Any, Optional
+from typing import Any
 
 import gtts
 import pyttsx3
@@ -10,24 +12,24 @@ from sanruum.utils.logger import logger
 
 
 class TextToSpeech:
-    def __init__(self, engine: str = "pyttsx3", language: str = "en") -> None:
+    def __init__(self, engine: str = 'pyttsx3', language: str = 'en') -> None:
         self.engine: str = engine
         self.language: str = language
-        self.speaker: Optional[Any] = None
+        self.speaker: Any | None = None
 
-        if self.engine == "pyttsx3":
+        if self.engine == 'pyttsx3':
             self.speaker = pyttsx3.init()
-            self.speaker.setProperty("rate", 150)
-            self.speaker.setProperty("volume", 1.0)
+            self.speaker.setProperty('rate', 150)
+            self.speaker.setProperty('volume', 1.0)
 
     def speak(self, text: str) -> None:
         """Convert text tp speech."""
-        if self.engine == "pyttsx3":
+        if self.engine == 'pyttsx3':
             self._speak_pyttsx3(text)
-        elif self.engine == "gtts":
+        elif self.engine == 'gtts':
             self._speak_gtts(text)
         else:
-            logger.error(f"Unsupported TTS engine: {self.engine}")
+            logger.error(f'Unsupported TTS engine: {self.engine}')
 
     def _speak_pyttsx3(self, text: str) -> None:
         """Offline TTS using pyttsx3."""
@@ -40,10 +42,11 @@ class TextToSpeech:
         tts = gtts.gTTS(text, lang=self.language)
         # Create a temporary file
         with tempfile.NamedTemporaryFile(
-                delete=False, suffix=".mp3"
+                delete=False,
+                suffix='.mp3',
         ) as temp_file:
             filename = temp_file.name
             tts.save(filename)
-            os.system(f"mpg321 {filename}")
+            os.system(f'mpg321 {filename}')
             # Cleanup the temporary file after playing it
             os.remove(filename)
