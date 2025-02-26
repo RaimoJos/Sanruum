75e5a54ddffed48797e0fc2bc255171f20708258
[RaimoJos] <[sanruumou@gmail.com]>
2025-02-26 16:36:19 +0200

    updated.

---
+++ sanruum/utils/health_tools/food_tools.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+
+def recommend_dietary_adjustments(current_diet: dict) -> None:
+    """Placeholder for diet adjustment recommendations."""
+    pass
+
+
+def suggest_low_glycemic_meals() -> None:
+    """Returns a list of low-glycemic meals."""
+    pass
