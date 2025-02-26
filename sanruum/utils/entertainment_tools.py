f2900ebd7bd76d9ebd91d83c7083063c6348e3f4
[RaimoJos] <[sanruumou@gmail.com]>
2025-02-26 16:40:09 +0200

    updated.

---
+++ sanruum/utils/entertainment_tools.py
@@ -0,0 +1,28 @@
+# Jokes, Quotes & Fun Facts
+from __future__ import annotations
+
+
+def tell_joke() -> None:
+    """Tells a random joke."""
+    pass
+
+
+def get_famous_quote() -> None:
+    """Returns a random famous quote."""
+    pass
+
+
+def get_motivational_quote() -> None:
+    """Returns a motivational quote to inspire the user."""
+    pass
+
+
+# Games & Trivia
+def get_riddle() -> None:
+    """Provides a random riddle and its answer."""
+    pass
+
+
+def get_trivia_question() -> None:
+    """Provides a random trivia question and possible answers."""
+    pass
