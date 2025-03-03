2a4e4ff0178afd52678cefea55d3d1c71ecc8d98
[RaimoJos] <[sanruumou@gmail.com]>
2025-03-03 18:44:11 +0200

    updated:

--- sanruum/config/__init__.py
+++ sanruum/config/__init__.py
@@ -1,8 +1,7 @@
 from __future__ import annotations
 
-from .base import BaseConfig
 from .project import ProjectDirectories
 from .project import ProjectFiles
 from .project import ProjectManager
 
-__all__ = ['BaseConfig', 'ProjectDirectories', 'ProjectFiles', 'ProjectManager']
+__all__ = ['ProjectDirectories', 'ProjectFiles', 'ProjectManager']
