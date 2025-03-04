6deaac8d0bdfa7e73c23c983e0d4fa68f784de5a
[RaimoJos] <[sanruumou@gmail.com]>
2025-03-04 09:09:08 +0200

    database.

--- sanruum/database/core/db.py
+++ sanruum/database/core/db.py
@@ -1,26 +1,7 @@
 from __future__ import annotations
 
-from sqlalchemy import create_engine
-from sqlalchemy.orm import scoped_session
-from sqlalchemy.orm import sessionmaker
+import os
 
-from sanruum.config import BaseConfig
-from sanruum.database.core.base import Base
-
-DATABASE_URL = BaseConfig.DB_URL
-
-engine = create_engine(
-    DATABASE_URL, echo=False, future=True,
-)
-
-SessionLocal = scoped_session(
-    sessionmaker(
-        autocommit=False, autoflush=False, bind=engine,
-    ),
-)
-
-
-# Initialize the database
-def init_db() -> None:
-    """Create database tables."""
-    Base.metadata.create_all(bind=engine)
+dbname = os.getenv('DATABASE_NAME')
+user = os.getenv('DATABASE_USERNAME')
+password = os.getenv('DATABASE_PASSWORD')
